eb292d8760d43e6f23e6c68817ae337ade4798d4
Preston Fawcett <ptfawcett@gmail.com>
2022-09-12 12:10:22 -0700

    Update passwdReq.py
    Implemented expiration period for newly created users. Need user list to update pre-existing users

--- passwdReq.py
+++ passwdReq.py
@@ -2,13 +2,32 @@ import os
 import sys
 import subprocess
 
-def passwdReqs(minlength, upper, lower, digit, other):
-    parameters = 's/^password\trequisite\t\t\tpam_pwquality.so.*$/password\trequisite\t\t\tpam_pwquality.so retry=3 ' + minlength + " " + upper + " " + lower + " " + digit + " " + other + '/'
-    subprocess.run(["perl", "-p", "-i.orig", "-e", parameters, "/etc/pam.d/common-password"])        
-
 def passwdReqPromp():
+    option = ""
+    while option != "0":
+        option = input("------------------------------------\n"
+                       "Options\n"
+                       "------------------------------------\n"
+                       "1. Change password complexity\n"
+                       "2. Change password expiration period\n"
+                       "0. Back\n"
+                       "------------------------------------\n"
+                       "Select an option: ")
+        print("------------------------------------\n")
+
+        match option:
+            case "1":
+                passwdComplex()
+            case "2":
+                passwdExpir()
+            case "0":
+                print("Back to main menu.")
+            case _:
+                print("Invalid entry.")                
+
+def passwdComplex():
     print("Change Password Requirements\n")
-    print("----------------------------\n\n")
+    print("----------------------------\n")
     minlength = "minlen=" + input("Minimun length of Password: ")
     answer = "n"
     upper = 'ucredit=0'
@@ -29,3 +48,28 @@ def passwdReqPromp():
         other = "ocredit=-1"
 
     passwdReqs(minlength, upper, lower, digit, other)
+
+def passwdReqs(minlength, upper, lower, digit, other):
+    parameters = 's/^password\trequisite\t\t\tpam_pwquality.so.*$/password\trequisite\t\t\tpam_pwquality.so retry=3 ' + minlength + " " + upper + " " + lower + " " + digit + " " + other + '/'
+    subprocess.run(["perl", "-p", "-i.orig", "-e", parameters, "/etc/pam.d/common-password"])        
+
+def passwdExpir():
+    print("Change Password Expiration period\n")
+    print("----------------------------------\n")
+    maxDay = "99999"
+    minDay = "0"
+    warn = "7"
+
+    maxDay = input("Maximum number of days password may be used: ")
+    minDay = input("Minimum number of days allowed between password changes: ")
+    warn = input("Number of days warning given before password expires: ")
+
+    passwdExpirConfig(maxDay, minDay, warn)
+
+def passwdExpirConfig(maxDay, minDay, warn):
+    parameter = "s/^PASS_MAX_DAYS\t.*$/PASS_MAX_DAYS\t" + maxDay + "/"
+    subprocess.run(["perl", "-p", "-i.orig", "-e", parameter, "/etc/login.defs"])
+    parameter = "s/^PASS_MIN_DAYS\t.*$/PASS_MIN_DAYS\t" + minDay + "/"
+    subprocess.run(["perl", "-p", "-i.orig", "-e", parameter, "/etc/login.defs"])
+    parameter = "s/^PASS_WARN_AGE\t.*$/PASS_WARN_AGE\t" + warn + "/"
+    subprocess.run(["perl", "-p", "-i.orig", "-e", parameter, "/etc/login.defs"])
