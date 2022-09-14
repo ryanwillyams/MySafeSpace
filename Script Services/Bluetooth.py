db04dcbe380abc0765d57ee3b96963b4b687f949
Ryan williams <70562805+ryanwillyams@users.noreply.github.com>
2022-09-13 19:26:34 -0700

    Services Scripts
    Able to disable certain services through scripts. May add more as needed.

---
+++ Script Services/Bluetooth.py
@@ -0,0 +1,6 @@
+from subprocess import PIPE, Popen
+
+command = "sudo systemctl disable bluetooth"
+with Popen(command, stdout=PIPE, stderr=None, shell=True) as process:
+    output = process.communicate()[0].decode("utf-8")
+    print(output)
---
+++ Script Services/executeServiceDisability.py
@@ -0,0 +1,4 @@
+import subprocess
+exec(open("Bluetooth.py").read())
+exec(open("Printer.py").read())
+exec(open("SSHD.py").read())
