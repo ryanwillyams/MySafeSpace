3f8117cc078005c443cfbcae8427e49cd162af26
Preston Fawcett <ptfawcett@gmail.com>
2022-10-04 00:03:04 -0700

    Changed visuals
    Hardening page now has a scroll bar instead of tabs

--- GUI/Views/HardenPage.py
+++ GUI/Views/HardenPage.py
@@ -28,44 +28,51 @@ class HardenPage(QWidget):
     def __init__(self):
         super(HardenPage,self).__init__()
         
-        layout = QVBoxLayout()
+        # Layout
+        self.layout = QGridLayout(self)
 
-        tabs = QTabWidget()
-        # list_widget = QListWidget()
-        # list_widget.setGeometry()
-        tabs.setTabPosition(QTabWidget.TabPosition.West)
+        # Scrollbar
+        list_widget = QListWidget()
+        list_widget.setMaximumWidth(180)
 
-        # Initialize tabs
-        passReqTab = PasswordReqTab()
-        changePassTab = ChangePasswordTab()
-        changeSudoers = ChangeSudoers()
-        disableServices = DisableServices()
-        iptables = IPTables()
-        # passReqTab = QListWidgetItem("Password Requirements")
-        # changePassTab = QListWidgetItem("Change Password")
-        # changeSudoers = QListWidgetItem("Change Sudoers")
-        # disableServices = QListWidgetItem("Disable Services")
-        # iptables = QListWidgetItem("IPTables")
+        # Scrollbar List
+        passReqTab = QListWidgetItem("Password Requirements")
+        changePassTab = QListWidgetItem("Change Password")
+        changeSudoers = QListWidgetItem("Change Sudoers")
+        disableServices = QListWidgetItem("Disable Services")
+        iptables = QListWidgetItem("IPTables")
+        self.current_tab = PasswordReqTab()
 
-
-        tabs.addTab(passReqTab,"Password Requirements")
-        tabs.addTab(changePassTab,"Change Password")
-        tabs.addTab(changeSudoers, "Change Sudoers")
-        tabs.addTab(disableServices, "Disable Services")
-        tabs.addTab(iptables, "IPTables")
-        # list_widget.addItem(passReqTab)
-        # list_widget.addItem(changePassTab)
-        # list_widget.addItem(changeSudoers)
-        # list_widget.addItem(disableServices)
-        # list_widget.addItem(iptables)
-        # list_widget.itemClicked.connect(PasswordReqTab.__init__)
+        list_widget.addItem(passReqTab)
+        list_widget.addItem(changePassTab)
+        list_widget.addItem(changeSudoers)
+        list_widget.addItem(disableServices)
+        list_widget.addItem(iptables)
+        list_widget.itemClicked.connect(self.change_tab)
         
-        layout.addWidget(tabs)
-        # scroll_bar = QScrollBar(self)
-        # list_widget.setVerticalScrollBar(scroll_bar)
-        # layout.addWidget(list_widget)
+        # Scrollbar formatting
+        scroll_bar = QScrollBar(self)
+        list_widget.setVerticalScrollBar(scroll_bar)
+        self.layout.addWidget(list_widget, 0, 0)
+        self.layout.addWidget(self.current_tab, 0, 1)
 
-        self.setLayout(layout)
+        self.setLayout(self.layout)
+    
+    # Function for changing between different menus
+    def change_tab(self, item):
+        self.current_tab.close()
+        match item.text():
+            case "Password Requirements":                
+                self.current_tab = PasswordReqTab()
+            case "Change Password":
+                self.current_tab = ChangePasswordTab()                
+            case "Change Sudoers":                
+                self.current_tab = ChangeSudoers()                
+            case "Disable Services":                
+                self.current_tab = DisableServices()                
+            case "IPTables":                
+                self.current_tab = IPTables()                
+        self.layout.addWidget(self.current_tab, 0, 1)
 
 #TODO Add better Styling to password requirements
 # - Better Text Area
@@ -78,6 +85,7 @@ class PasswordReqTab(QWidget):
     def __init__(self):
         super(PasswordReqTab,self).__init__()
         main_layout = QGridLayout(self)
+        
         ##
         #Initalize Minimum Characters
         self.minCharLabel = QLabel('&Minimum password length', self)
