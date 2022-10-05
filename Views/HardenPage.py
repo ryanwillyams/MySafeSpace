c6a4c647f224ea567fe364287f7a7f8b99e4eed2
Preston Fawcett <ptfawcett@gmail.com>
2022-10-05 13:12:42 -0700

    Update HardenPage.py
    Implemented ChangeSudoers class

--- Views/HardenPage.py
+++ Views/HardenPage.py
@@ -10,9 +10,10 @@ from PyQt6.QtCore import Qt
 import sys 
 
 sys.path.append("..")
-from functions import listUsers
+from functions import listUsers, list_sudoers, list_nonsudoers
 from passwdReq import passwdExpirConfig, passwdReqs
 from change_passwds import passwdChange
+from sudo_priv import addSudo, removeSudo
 
 """
 Options for harden tab
@@ -245,6 +246,67 @@ class ChangePasswordTab(QWidget):
 class ChangeSudoers(QWidget):
     def __init__(self):
         super(ChangeSudoers,self).__init__()
+        # Declare Layouts
+        outer_layout = QVBoxLayout()
+        top_layout = QGridLayout()
+        bottom_layout = QHBoxLayout()
+
+        # Sudoer label and list
+        sudoer_label = QLabel("Sudoers")
+        self.sudoer_list_display = QListWidget()
+        sudoer_list = list_sudoers()
+        for sudoer in sudoer_list:
+            item = QListWidgetItem(sudoer)
+            item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            self.sudoer_list_display.addItem(item)
+
+        # Add sudo Widgets into layout
+        top_layout.addWidget(sudoer_label, 0, 0)
+        top_layout.addWidget(self.sudoer_list_display, 1, 0)
+
+        # Normal User label and list
+        normal_users_label = QLabel("Normal Users")
+        self.normal_user_list_display = QListWidget()
+        normal_user_list = list_nonsudoers()
+        for user in normal_user_list:
+            item = QListWidgetItem(user)
+            item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            self.normal_user_list_display.addItem(item)
+
+        # Add normal user widgets into layout
+        top_layout.addWidget(normal_users_label, 0, 1)
+        top_layout.addWidget(self.normal_user_list_display, 1, 1)
+
+        # Submit button
+        submit_button = QPushButton("Submit", clicked=self.changeCheckedUsers)
+        bottom_layout.addWidget(submit_button)
+
+        # Add sublayouts to main layout
+        outer_layout.addLayout(top_layout)
+        outer_layout.addLayout(bottom_layout)
+
+        # Set windows main layout
+        self.setLayout(outer_layout)
+
+    # Changes users between sudo and normal users
+    def changeCheckedUsers(self):
+        # Fetch all checked sudoers and remove from sudo group
+        checked_sudoers = []
+        for index in range(self.sudoer_list_display.count()):
+            if self.sudoer_list_display.item(index).checkState() == Qt.CheckState.Checked:
+                checked_sudoers.append(self.sudoer_list_display.item(index).text())
+
+        removeSudo(checked_sudoers)
+
+        # Fetch all checked normal users and add to sudo group
+        checked_normals = []
+        for index in range(self.normal_user_list_display.count()):
+            if self.normal_user_list_display.item(index).checkState() == Qt.CheckState.Checked:
+                checked_normals.append(self.normal_user_list_display.item(index).text())
+
+        addSudo(checked_normals)
 
 class DisableServices(QWidget):
     def __init__(self):
