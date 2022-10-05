7857289bffb238035a5d8a4b8f8fb9f7cb627daa
Cody Mangham <cmangham1@csu.fullerton.edu>
2022-10-04 19:56:45 -0700

    Fixed Styling

--- GUI/Views/HardenPage.py
+++ GUI/Views/HardenPage.py
@@ -1,8 +1,8 @@
 from logging.handlers import QueueListener
 from PyQt6.QtWidgets import (
-    QWidget,QTabWidget,QFormLayout, QVBoxLayout, QHBoxLayout,
+    QWidget,QTabWidget,QFormLayout,QGridLayout, QVBoxLayout, QHBoxLayout,
     QLabel, QPushButton,QLineEdit,QCheckBox, QSpinBox,
-    QListWidget,QListWidgetItem
+    QListWidget,QListWidgetItem, QScrollBar
 )
 from PyQt6.QtCore import Qt
 
--- GUI/app.py
+++ GUI/app.py
@@ -9,16 +9,15 @@ if __name__ == '__main__':
 
     ##
     # Get style of application
-    with open('main.qss','r') as f:
-        default_dark_style = f.read()
+
     app = QApplication([])
 
     # Force the style to be the same on all OSs:
-    app.setStyleSheet(default_dark_style)
+    app.setStyleSheet("Fusion")
     
     #Set a default Dark theme
-    # palette = get_dark_color_theme()
-    # app.setPalette(palette)
+    palette = get_dark_color_theme()
+    app.setPalette(palette)
 
 
     window = MainWindow()
