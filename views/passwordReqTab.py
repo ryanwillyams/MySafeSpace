f29b3ee30e7d2223454e641ee53a39e2819f4479
Preston Fawcett <ptfawcett@gmail.com>
2022-10-12 14:36:40 -0700

    Update passwordReqTab.py
    Changed layout to form layout, simplified and cleaned code

--- views/passwordReqTab.py
+++ views/passwordReqTab.py
@@ -1,8 +1,7 @@
-from logging.handlers import QueueListener
+
 from PyQt6.QtWidgets import (
-    QWidget,QTabWidget,QFormLayout,QGridLayout, QVBoxLayout, QHBoxLayout,
-    QLabel, QPushButton,QLineEdit,QCheckBox, QSpinBox, QComboBox,
-    QListWidget,QListWidgetItem, QScrollBar, QMessageBox
+    QWidget,QFormLayout, QVBoxLayout, QHBoxLayout,
+    QLabel, QPushButton,QCheckBox, QSpinBox
 )
 from PyQt6.QtCore import Qt
 
@@ -11,107 +10,70 @@ from scripts.passwdReq import passwdExpirConfig, passwdReqs
 class PasswordReqTab(QWidget):
     def __init__(self):
         super(PasswordReqTab,self).__init__()
-        main_layout = QGridLayout(self)
+        # Declare layouts
+        outer_layout = QVBoxLayout()
+        top_layout = QFormLayout()
+        btn_layout = QHBoxLayout()
+        btn_layout.setAlignment(Qt.AlignmentFlag.AlignRight)
         
-        ##
-        # Maximum days before password change
-        self.max_day_label = QLabel('&Maximum number of days before changing password', self)
+        # Define Top layout
         self.max_day = QSpinBox(self)
-        self.max_day_label.setBuddy(self.max_day)
-
         self.max_day.setRange(1,99999)
         self.max_day.setMaximumSize(80,32)
         self.max_day.setValue(90)
 
-        main_layout.addWidget(self.max_day_label, 0, 0)
-        main_layout.addWidget(self.max_day, 0, 1)
-
-        #Minimum days before password change
-        self.min_day_label = QLabel('&Minimum number of days before changing password', self)
         self.min_day = QSpinBox(self)
-        self.min_day_label.setBuddy(self.min_day)
-
         self.min_day.setRange(1,99999)
         self.min_day.setMaximumSize(80,32)
         self.min_day.setValue(30)
 
-        main_layout.addWidget(self.min_day_label, 1, 0)
-        main_layout.addWidget(self.min_day, 1, 1)
-
-        # Warning before password change
-        self.warning_label = QLabel('&Number of days before warning is given to change passsword', self)
         self.warning = QSpinBox(self)
-        self.warning_label.setBuddy(self.warning)
-
         self.warning.setRange(1,31)
         self.warning.setMaximumSize(48,32)
         self.warning.setValue(7)
 
-        main_layout.addWidget(self.warning_label, 2, 0)
-        main_layout.addWidget(self.warning, 2, 1)
-        
-        #Initalize Minimum Characters
-        self.minCharLabel = QLabel('&Minimum password length', self)
         self.min_chars = QSpinBox(self)
-        self.minCharLabel.setBuddy(self.min_chars)
-
         self.min_chars.setRange(4,32)
         self.min_chars.setMaximumSize(48,32)
         self.min_chars.setValue(12)
 
-        main_layout.addWidget(self.minCharLabel, 3, 0)
-        main_layout.addWidget(self.min_chars, 3, 1)
-
-        # Initalize Password History
-        self.passHist = QLabel('Number of previous passwords remembered', self)
         self.pass_remember = QSpinBox(self)
-        self.passHist.setBuddy(self.pass_remember)
-
         self.pass_remember.setRange(1,10)
         self.pass_remember.setMaximumSize(48,32)
         self.pass_remember.setValue(5)
 
-        main_layout.addWidget(self.passHist, 4, 0)
-        main_layout.addWidget(self.pass_remember, 4, 1)
-
-
-        ##
-        # Intialize Checked buttons
-        self.check_buttons = []
         self.need_upper_case = QCheckBox("Upper Case")
         self.need_lower_case = QCheckBox("Lower Case")
         self.need_digits = QCheckBox("Digits")
         self.need_special_chars = QCheckBox("Special Characters")
-        
-        self.check_buttons.append(self.need_upper_case)
-        self.check_buttons.append(self.need_lower_case)
-        self.check_buttons.append(self.need_digits)
-        self.check_buttons.append(self.need_special_chars)
 
         self.need_upper_case.setChecked(True)
         self.need_lower_case.setChecked(True)
         self.need_digits.setChecked(True)
         self.need_special_chars.setChecked(True)
 
-        index = 5
-        for button in self.check_buttons:
-            main_layout.addWidget(button, index, 0)
-            index += 1
+        # Add Widgets to Top layout
+        top_layout.addRow(QLabel('Maximum number of days before changing password'), self.max_day)
+        top_layout.addRow(QLabel('Minimum number of days before changing password'), self.min_day)
+        top_layout.addRow(QLabel('Number of days before warning is given to change passsword'), self.warning)
+        top_layout.addRow(QLabel('Minimum password length'), self.min_chars)
+        top_layout.addRow(QLabel('Number of previous passwords remembered'), self.pass_remember)
+        top_layout.addRow(self.need_upper_case)
+        top_layout.addRow(self.need_lower_case)
+        top_layout.addRow(self.need_digits)
+        top_layout.addRow(self.need_special_chars)
 
-        ##
-        # Initialize Buttons
-        button_layout = QHBoxLayout()
-        self.clear_button = QPushButton("Clear")
-        self.submit_button = QPushButton("Submit", clicked=self.submit_password_req_changes)
+        # Define Btn layout
+        self.apply_btn = QPushButton("Apply", clicked=self.submit_password_req_changes)
 
-        button_layout.addWidget(self.clear_button)
-        button_layout.addWidget(self.submit_button)
+        btn_layout.addWidget(self.apply_btn)
 
-        bottom_buttons = QWidget()
-        bottom_buttons.setLayout(button_layout)
-        
-        main_layout.addWidget(bottom_buttons)
-        self.setLayout(main_layout)
+        # Add sublayouts to main layout
+        outer_layout.addLayout(top_layout)
+        outer_layout.addLayout(btn_layout)
+
+        # Set window's main layout
+        self.setLayout(outer_layout)
 
 
     def submit_password_req_changes(self):
