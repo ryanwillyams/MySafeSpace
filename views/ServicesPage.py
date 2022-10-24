c11f32a9c8c8edc7d785f351b72f66c8de9b0191
Preston <ptfawcett@gmail.com>
2022-10-23 21:52:51 -0700

    Implemented base design

--- views/ServicesPage.py
+++ views/ServicesPage.py
@@ -1,7 +1,39 @@
-from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import (
+    QWidget,QTabWidget,QFormLayout,QGridLayout, QVBoxLayout, QHBoxLayout,
+    QLabel, QPushButton,QLineEdit,QCheckBox, QSpinBox, QComboBox,
+    QListWidget,QListWidgetItem, QScrollBar, QMessageBox, QTreeView
+)
+from PyQt6.QtCore import Qt
 
-class ServicesPage(QtWidgets.QWidget):
+from views.systemCare import SystemCare
+from views.automaticBackup import AutomaticBackup
+
+class ServicesPage(QWidget):
     
     def __init__(self):
         super(ServicesPage,self).__init__()
-        pass
+        
+        # Declare layout
+        outer_layout = QHBoxLayout()
+
+        # Define layout
+        services = QTabWidget()
+        services.setMovable(False)
+
+        # Declare tabs
+        system_care = SystemCare()
+        auto_backup = AutomaticBackup()
+
+        tab_views = {
+            'System Care' : system_care,
+            'Automatic Backup' : auto_backup
+        }
+
+        for view_name,view in tab_views.items():
+            services.addTab(view,view_name)
+
+        # Add widget to layout
+        outer_layout.addWidget(services)
+
+        # Set layout to main window
+        self.setLayout(outer_layout)
