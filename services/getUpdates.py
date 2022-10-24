88949ebfee770ee238acd3efef7b0ab7b563b424
Preston <ptfawcett@gmail.com>
2022-10-23 21:52:02 -0700

    Made function callable

--- services/getUpdates.py
+++ services/getUpdates.py
@@ -2,5 +2,9 @@
 
 import os
 
-cmd = 'sudo apt-get update'
-os.system(cmd)
+# cmd = 'sudo apt-get update'
+# os.system(cmd)
+
+def update():
+    cmd = "sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y"
+    os.system(cmd)
