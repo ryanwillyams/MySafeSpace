5088b24322f00a6141b4807843c85bf3efde8c18
Cody Mangham <cody.w.mangham@gmail.com>
2022-12-12 16:35:08 -0800

    fixed yaml file for qt

--- test/test_main_window.py
+++ test/test_main_window.py
@@ -5,21 +5,20 @@ from PyQt6.QtWidgets import QDialog, QFileDialog
 
 from views.MainWindow import MainWindow
 
-
-new_window = MainWindow()
-new_window.show()
-
-
-def test_arithemtic_works():
-    """Check that the computer isn't bad"""
-    assert 1 + 1 == 2
+@pytest.fixture
+def window(qtbot):
+    """Pass the application to the test functions via a pytest fixture."""
+    new_window = MainWindow()
+    qtbot.add_widget(new_window)
+    new_window.show()
+    return new_window
 
 def test_window_title(window):
     """Check that the window title shows as declared."""
     assert window.windowTitle() == 'My Safe Space'
 
 
-# def test_window_geometry(window):
-#     """Check that the window width and height are set as declared."""
-#     assert window.width() == 1024
-#     assert window.height() == 768
+def test_window_geometry(window):
+    """Check that the window width and height are set as declared."""
+    assert window.width() == 1024
+    assert window.height() == 768
